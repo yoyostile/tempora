1f878b6ed99140a40764fb46f4e1525c9cab089e
Johannes Heck <yoyostile@gmail.com>
2013-07-31 00:39:15 +0200

    strange stuff

--- spec/tempora/evaluation/evaluation_spec.rb
+++ spec/tempora/evaluation/evaluation_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "Evaluation", broken: true do
+describe "Evaluation" do#, broken: true do
 
   before(:all) do
     file = File.open("spec/tempora/evaluation/jester.csv")
@@ -21,7 +21,7 @@ describe "Evaluation", broken: true do
       end
       p j
       j += 1
-      break if j == 100
+      break if j > 1000
     end
     Tempora::Logging::Core.persist_hash gl_ratings
   end
@@ -40,23 +40,24 @@ describe "Evaluation", broken: true do
   it "should be able to predict", skip_before: true do
     LOGGER_REG = /(?<logger>\w+)::(?<logger_id>\d+)/
     y = []
-    y2 = []
     length = User.count
     threads = []
     i = 0
-    User.find_in_batches(batch_size: 4) do |user_batch|
-      i += 1
+    User.find_in_batches(batch_size: 8) do |user_batch|
       user_batch.each do |user|
         threads << Thread.new {
           ratings = Tempora.redis.hgetall(Tempora::KeyMapper.logger_key(user))
+          i += 1 
           p "#{Thread.current} :: [#{i} / #{length}] :: Processing..."
           ratings.each_with_index do |first_rating, j|
             artist = Artist.find(first_rating[0].match(LOGGER_REG)[:logger_id])
+            hash = {}
             org_rating = first_rating[1]
-            y.push org_rating
+            hash[:org] = org_rating
             Tempora.redis.hdel(Tempora::KeyMapper.logger_key(user), first_rating[0])
             new_rating = Tempora::Recommender::Core.prediction(user, artist)
-            y2.push org_rating
+            hash[:new] = new_rating
+            y.push hash
             delta = (org_rating.to_f - new_rating.to_f).abs
             # p "#{Thread.current} :: [#{i} / #{length}] :: [#{j} / #{ratings.length}] :: #{org_rating} => #{new_rating} :: Delta: #{delta}"
           end
@@ -68,13 +69,14 @@ describe "Evaluation", broken: true do
     nominator = 0
     denominator = 0
     y.each_with_index do |v, i|
-      nominator += (y[i].to_f + y2[i].to_f)**2
+      nominator += (v[:old].to_f - v[:new].to_f)**2
       denominator += 1
     end
-    rsme = Math.sqrt(nominator / denominator)
+    rmse = Math.sqrt(nominator / denominator)
     p "User.count #{User.count}"
     p "Artist.count #{Artist.count}"
-    p "RSME: #{rsme}"
+    p "Ratings.count #{y.length}"
+    p "RMSE: #{rmse}"
   end
 
 end
