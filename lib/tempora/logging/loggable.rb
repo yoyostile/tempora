95c6db45f91c01751d603533037337f9601b9e83
Johannes Heck <johannes@endore.me>
2013-08-06 14:41:07 +0200

    modularization

--- lib/tempora/logging/loggable.rb
+++ lib/tempora/logging/loggable.rb
@@ -5,20 +5,6 @@
 
       module ClassMethods
         def is_loggable?
-          false
-        end
-
-        # Sets needed has_many association, includes and extends.
-        # @param opts {} is optional
-        def acts_as_loggable(opts={})
-          has_many :logs, as: :loggable, class_name: "Tempora::Logging::Log"
-          include LoggableMethods
-          extend LoggableClassMethods
-        end
-      end
-
-      module LoggableClassMethods
-        def is_loggable?
           true
         end
 
@@ -29,30 +15,28 @@
       end
 
 
-      module LoggableMethods
-        def is_loggable?
-          true
-        end
+      def is_loggable?
+        true
+      end
 
-        def is_logger?
-          false
-        end
+      def is_logger?
+        false
+      end
 
-        def type
-          self.class.to_s
-        end
+      def type
+        self.class.to_s
+      end
 
-        # Is self associated with logger?
-        # @param logger
-        # @return [Boolean]
-        def assoc_with? logger
-          if logger.is_logger?
-            assoc = logger.send(logger.class.loggable_assoc.select{
-              |a| a.klass == self.class
-            }.first.plural_name).find self rescue nil
-          end
-          assoc.present?
+      # Is self associated with logger?
+      # @param logger
+      # @return [Boolean]
+      def assoc_with? logger
+        if logger.is_logger?
+          assoc = logger.send(logger.class.loggable_assoc.select{
+            |a| a.klass == self.class
+          }.first.plural_name).exists? self rescue nil
         end
+        assoc.present?
       end
     end
   end
--- lib/tempora/logging/logger.rb
+++ lib/tempora/logging/logger.rb
@@ -5,20 +5,6 @@ module Tempora
 
       module ClassMethods
         def is_logger?
-          false
-        end
-
-        # Sets needed has_many association, includes and extends.
-        # @param opts {} is optional
-        def acts_as_logger(opts={})
-          has_many :logs, as: :logger, class_name: "Tempora::Logging::Log"
-          include LoggerMethods
-          extend LoggerClassMethods
-        end
-      end
-
-      module LoggerClassMethods
-        def is_logger?
           true
         end
 
@@ -28,62 +14,60 @@ module Tempora
         end
       end
 
-      module LoggerMethods
 
-        # Creates a new log entry for self and the given loggable
-        # @param loggable item to log
-        # @param opts {}
-        def log(loggable, opts={})
-          return false unless loggable.respond_to?(:is_loggable?) && loggable.is_loggable?
-          logs.create loggable: loggable, event: "#{loggable.class.to_s}::#{opts[:event]}"
+      # Creates a new log entry for self and the given loggable
+      # @param loggable item to log
+      # @param opts {}
+      def log(loggable, opts={})
+        return false unless loggable.respond_to?(:is_loggable?) && loggable.is_loggable?
+        logs.create loggable: loggable, event: "#{loggable.class.to_s}::#{opts[:event]}"
+      end
+
+      def ratings
+        Tempora.redis.hgetall Tempora::KeyMapper.logger_key self
+      end
+
+      def similarity_with logger
+        Tempora::Recommender::Core.similarity self, logger
+      end
+
+      def predict loggable
+        Tempora::Recommender::Core.prediction self, loggable
+      end
+
+      def is_logger?
+        true
+      end
+
+      def is_loggable?
+        false
+      end
+
+      # Is self associated with loggable?
+      # @param loggable
+      # @return [Boolean]
+      def assoc_with? loggable
+        if loggable.is_loggable?
+          assoc = loggable.send(loggable.class.logger_assoc.select{
+            |a| a.klass == self.class
+          }.first.plural_name).find self rescue nil
         end
+        assoc.present?
+      end
 
-        def ratings
-          Tempora.redis.hgetall Tempora::KeyMapper.logger_key self
-        end
+      # @param limit [Integer]
+      # @return [Array] List with recommended items
+      def recommendation_list limit = 10
+        Tempora::Recommender::Core.recommendation_list self, limit
+      end
 
-        def similarity_with logger
-          Tempora::Recommender::Core.similarity self, logger
-        end
-
-        def predict loggable
-          Tempora::Recommender::Core.prediction self, loggable
-        end
-
-        def is_logger?
-          true
-        end
-
-        def is_loggable?
-          false
-        end
-
-        # Is self associated with loggable?
-        # @param loggable
-        # @return [Boolean]
-        def assoc_with? loggable
-          if loggable.is_loggable?
-            assoc = loggable.send(loggable.class.logger_assoc.select{
-              |a| a.klass == self.class
-            }.first.plural_name).find self rescue nil
-          end
-          assoc.present?
-        end
-
-        # @param limit [Integer]
-        # @return [Array] List with recommended items
-        def recommendation_list limit = 10
-          Tempora::Recommender::Core.recommendation_list self, limit
-        end
-
-        # @param loggable_class your loggable model
-        # @return [Array] associated items
-        def association_list loggable_class
-          if loggable_class.is_loggable?
-            assoc = self.send(self.class.loggable_assoc.select{
-              |a| a.klass == loggable_class
-            }.first.plural_name)
-          end
+      # @param loggable_class your loggable model
+      # @return [Array] associated items
+      def association_list loggable_class
+        if loggable_class.is_loggable?
+          assoc = self.send(self.class.loggable_assoc.select{
+            |a| a.klass == loggable_class
+          }.first.plural_name)
         end
       end
     end
